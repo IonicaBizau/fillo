0a1b46c36f1ffa430cc93bad48ba4858fb78305b
Ionică Bizău <bizauionica@gmail.com>
2015-12-20 09:08:21 +0200

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,11 @@
+const fillo = require("../lib");
+
+// Fill with with zeros (default behavior)
+console.log(fillo(7));
+// => "07"
+
+console.log(fillo(42, 5));
+// => "00042"
+
+console.log(fillo(42, 4, "#"));
+// => "##42"
